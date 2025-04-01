c4b7d942818decf9c4cba0666fcd3f2271a04a38
diegoMercado02 <diegomercado2002@gmail.com>
2025-04-02 01:31:04 +0200

    feat: enable middleware

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,7 +1,15 @@
 import type { Config } from "@react-router/dev/config";
 
+declare module "react-router" {
+  interface Future {
+    unstable_middleware: true; // 👈 Enable middleware types
+  }
+}
+
 export default {
-  // Config options...
+  future: {
+    unstable_middleware: true, // 👈 Enable middleware
+  },
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true,
 } satisfies Config;
